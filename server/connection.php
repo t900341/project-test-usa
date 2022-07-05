754bd3cf0c35fbb76e9ba3fa0b5ac1d88d298162
unknown <t900341@gmail.com>
2022-07-05 11:54:19 +0900

    test 0705

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("localhost","root","","shopping")
+$conn = mysqli_connect("usa-rds.cjgausilhpys.us-east-1.rds.amazonaws.com","root","ekfl1005","shopping")
         or die("Couldn't connect to database");
 
 
